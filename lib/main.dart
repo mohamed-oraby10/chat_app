64f3aec894e591ef885e3b096e256615c26ecda4
hp <mohamedoraby0500@gmail.com>
2025-10-22 17:58:56 +0300

    create simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/screens/blocs/auth_cubit/auth_bloc.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:chat_app/screens/cubits/auth_cubit/auth_cubit.dart';
 import 'package:chat_app/screens/cubits/chat_cubit/chat_cubit.dart';
+import 'package:chat_app/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
@@ -12,6 +13,7 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const ChatApp());
 }
 
@@ -24,10 +26,8 @@ class ChatApp extends StatelessWidget {
       providers: [
         // BlocProvider(create: (context) => LoginCubit()),
         BlocProvider(create: (context) => AuthBloc()),
-                BlocProvider(create: (context) => ChatCubit()),
-                 BlocProvider(create: (context) => AuthCubit()),
-
-
+        BlocProvider(create: (context) => ChatCubit()),
+        BlocProvider(create: (context) => AuthCubit()),
       ],
       child: MaterialApp(
         routes: {
